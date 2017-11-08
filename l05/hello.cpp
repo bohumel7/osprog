3fb9da17c747fa6da4fb897188efcd59f4fa4c3e
Jozef Šiška <siska@ii.fmph.uniba.sk>
2017-11-08 16:28:40 +0100

    l05 hello: implement actual greeting

--- l05/hello.cpp
+++ l05/hello.cpp
@@ -2,12 +2,107 @@
 #include <cstdio>
 #include <cstdint>
 #include <cerrno>
+#include <cstring>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h> // inet_ntoa
 
 void err(const char *msg) { perror(msg); exit(EXIT_FAILURE); }
+
+/**
+ * Reads a line (terminated by \n) from fd,
+ * but only up to maxSize bytes.
+ *
+ * @returns number of bytes read (incl. \n if it was present
+ * in first maxSize bytes).
+ *
+ * Note: there is now way to find out how much data was actually read
+ * before an error occured.
+ */
+ssize_t readLine(int fd, char *buffer, size_t maxSize) {
+	size_t bytesRead = 0;
+	while(bytesRead < maxSize) {
+		ssize_t count = read(fd, buffer, maxSize - bytesRead);
+		if (count == -1) {
+			if (errno == EINTR)
+				continue;
+			else
+				return -1;
+		}
+		if (count == 0)
+			return bytesRead; // EOF
+		for (int i = 0; i < count; ++i) {
+			if (buffer[bytesRead + i] == '\n')
+				return bytesRead + i;
+		}
+	}
+	return bytesRead;
+}
+
+/**
+ * Writes until all the data are written or an arror
+ * is encountered.
+ *
+ * @returns either size or -1
+ *
+ * Note: there is now way to find out how much data was actually written
+ * before an error occured.
+ */
+ssize_t writeAll(int fd, const char *buffer, size_t size) {
+	size_t bytesWritten = 0;
+	while (bytesWritten < size) {
+		ssize_t count = write(fd, buffer + bytesWritten, size - bytesWritten);
+		if (count == -1) {
+			if (errno == EINTR)
+				continue;
+			else
+				return -1;
+		}
+		bytesWritten += count;
+	}
+	return bytesWritten;
+}
+
+/*
+ * basically equivalent to this python:
+ *     line = sock.readLine(60).
+ *     sock.write('Hello ' + line.rstrip('\n') + '!\n')
+ */
+void greetClient(int sock)
+{
+
+	char buffer[70] = "Hello ";  // Need space for "Hello " + max 60 bytes + "!\n"
+
+	size_t pos = strlen(buffer);
+	ssize_t lineSize = readLine(sock, buffer + pos, sizeof(buffer) - pos);
+	if (lineSize == -1) {
+		perror("Error reading from client");
+		close(sock);
+		return;
+	}
+
+	pos += lineSize;
+	buffer[pos++] = '!';
+	buffer[pos++] = '\n';
+
+	size_t bytesWritten = 0;
+	while (bytesWritten < pos) {
+		ssize_t count = write(sock, buffer + bytesWritten, pos - bytesWritten);
+		if (count == -1) {
+			if (errno == EINTR)
+				continue;
+			else {
+				perror("Error writing to client");
+				close(sock);
+				return;
+			}
+		}
+		bytesWritten += count;
+	}
+	close(sock);
+}
 
 int main(int argc, char* argv[])
 {
@@ -50,7 +145,8 @@ int main(int argc, char* argv[])
 			fprintf(stderr, "New connection from %s:%d\n",
 					inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
 
-			// TODO handle connection
+			// TODO asynchronously!
+			greetClient(clientSock);
 		}
 	}
 }
